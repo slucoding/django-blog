8ff1a5ed5dd25736f85a311a0aa26fc91c7969a6
slucoding <slucoding@gmail.com>
2020-06-10 07:13:07 +0600

    Page navigation: 1.Class PostListView with navigation created: blog/views.py. 2.Changed url-template: blog/urls.py. 3.Changed HTML: post/list.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 2)  # По 2 статьи на каждой странице.
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # Если страница не является целым числом, возвращаем первую страницу.
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
-        posts = paginator.page(paginator.num_pages)
-    return render(request,'blog/post/list.html', {'page': page, 'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
