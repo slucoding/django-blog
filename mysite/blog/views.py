61d57025103b7131b1d2cf08de050af040524d2e
slucoding <slucoding@gmail.com>
2020-06-12 22:15:39 +0600

    Share posts via e-mail: 1.Class EmailPostForm & file forms.py created. 2.Created a form handler: views.py. 3.Added url template: blog/urls.py. 4.Created html-form template: blog/templates/blog/post/share.html. 5.Added link code: detail.html. 6.Added GMail SMTP server data: mysite/settings.py. 7.GMail account settings are disabled, which prevent the use of SMTP server.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -14,3 +16,26 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Получение статьи по идентификатору.
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # Форма была отправлена на сохранение.
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Все поля формы прошли валидацию.
+            cd = form.cleaned_data
+            # ... Отправка электронной почты.
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            subject = f"{cd['name']} ({cd['email']}) recommends you reading '{post.title}'"
+            # message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'], cd['comments'])
+            message = f"Read '{post.title}' at {post_url}\n\n{cd['name']}\'s comments:{cd['comments']}"
+            send_mail(subject, message, 'aksemary@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
