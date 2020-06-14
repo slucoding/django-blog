644f94b9d46fa424a6c170eac124ff3b74d11234
slucoding <slucoding@gmail.com>
2020-06-14 14:23:33 +0600

    Create post comments: 1.Creating a model class Comment: models.py 2.Registered the model on the admin page: admin.py. 3.A form has been created for sending and validating comments: form.py. 4.A handler for checking the form and saving the comment has been created: views.py 5.Display in article template created: detail.html. 6.Create styles for comments: static/css/blog.css

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
@@ -15,7 +15,21 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)  # Список активных комментариев для этой статьи.
+    new_comment = None
+    comment_form = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)  # Пользователь отправил комментарий.
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)  # Создаем комментарий, но пока не сохраняем в базе данных.
+            new_comment.post = post  # Привязываем комментарий к текущей статье.
+            new_comment.save()  # Сохраняем комментарий в базе данных.
+        else:
+            comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'new_comment': new_comment,
+                                                     'comment_form': comment_form})
 
 
 def post_share(request, post_id):
